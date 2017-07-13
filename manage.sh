21f73ab055a07b9d5391c92f815a9baec431871c
jbrawley <jake.brawley@gmail.com>
2017-07-12 22:41:36 -0400

    add google-chrome-stable to package list

--- manage.sh
+++ manage.sh
@@ -4,6 +4,15 @@
 #install oh-my-zsh
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+#install google-chrome-stable
+#addkey
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#set repository
+#sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+#install package
+# sudo apt-get update
+# sudo apt-get install google-chrome-stable
+
 
 #!/usr/bin/env bash
 
