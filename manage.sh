8becf5cab207f737b49c69d325c4cf0bd114a0c8
jbrawley <jake.brawley@gmail.com>
2017-07-12 22:55:56 -0400

    add postman to packages

--- manage.sh
+++ manage.sh
@@ -13,6 +13,11 @@
 # sudo apt-get update
 # sudo apt-get install google-chrome-stable
 
+#install postman
+#wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+#sudo tar -xzf postman.tar.gz -C /opt
+#rm postman.tar.gz
+#sudo ln -s /opt/Postman/Postman /usr/bin/postman
 
 #!/usr/bin/env bash
 
